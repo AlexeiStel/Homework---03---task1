0a24104e0eed5e00c965867ea4a8b8d76ba6169f
AlexStel <alexeistel@gmail.com>
2025-01-12 18:02:44 +0300

    fix: added using isInstallment

--- task-01.ts
+++ task-01.ts
@@ -6,12 +6,18 @@ interface PriceParams {
 }
 
 const totalPrice = ({ price, discount, isInstallment, months }: PriceParams): number => {
-    if (price < 0 || discount < 0 || discount > 100 || months < 0) {
-        throw new Error("Некоректно указана стоимость или скидка, или значение количества месяцев")
-        
+    if (price < 0 || discount < 0 || discount > 100) {
+        throw new Error("Некоректно указана стоимость или скидка")    
     }
 
-    return price * (1 - discount/100) / months;   
+    if (isInstallment) {
+        if (months < 0) {
+            throw new Error("Некоректно указана количество месяцев")    
+        }
+       
+        return price * (1 - discount/100) / months;      
+    }
+    return price * (1 - discount/100);      
 };
 
 const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
